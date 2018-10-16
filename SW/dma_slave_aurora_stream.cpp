aa5d2f0e74a6bdcd2878d6b78dc9d17017302cf3
xinyi zhang <31314652+xyzxinyizhang@users.noreply.github.com>
2018-10-16 16:28:53 -0400

    Update dma_slave_aurora_stream.cpp

--- SW/dma_slave_aurora_stream.cpp
+++ SW/dma_slave_aurora_stream.cpp
@@ -5,17 +5,12 @@
 void dma_slave_aurora_stream(
 	hls::stream<AXI_DMA_IO >  &in_stream,
 	hls::stream<AXI_VAL >  &out_stream
-//	const unsigned data_length_slave,
-//	const unsigned indicator_slave
 	)
 {
-
-//#pragma HLS DATAFLOW
 #pragma HLS INTERFACE ap_ctrl_none port=return
 #pragma HLS INTERFACE axis port=in_stream
 #pragma HLS INTERFACE axis port=out_stream
-//#pragma HLS INTERFACE s_axilite port=data_length_slave
-//#pragma HLS INTERFACE s_axilite port=indicator_slave
+
 
 
 AXI_VAL out_stream_;
@@ -32,8 +27,6 @@ AXI_VAL out_stream_;
 
 			AXI_DMA_IO temp = in_stream.read();
 			out_stream_=temp.data;
-//			out_stream_actual=out_stream_;
-
 			out_stream.write(out_stream_);
 			}
 		}
